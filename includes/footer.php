f8c4db8e8ca5547757c64d084eb05d732f4b169a
laica-becz <zepedabiancaellise@gmail.com>
2025-11-27 13:09:08 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        footer {
+            background-color: #e0b070;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body>
+    <footer>Created by: Bianca Ellise C. Zepeda &copy: 2025</footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+            background-color: #C19A6B;
+            line-height: 1.6;
+            background-image: url()
+        }
+        header {
+            background-color: #e0b070;
+            color: white;
+            padding: 15px 0;
+            text-align: center;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        h1, h2 {
+            margin: 0;
+            font-size: 3em;
+        }
+        ul {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #855E42;
+        }
+        ul li {
+            float: left;
+            list-style: none;
+        }
+        ul li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        ul li a:hover {
+            background-color: #927869;
+        }
+    </style>
+</head>
+<body>
+    <header>BBJ STORE</header>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Recipes</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</body>
+</html>
